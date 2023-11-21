5dd104a042c6cb2004e4968c94587ed3988064c9
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2023-11-21 12:15:13 +0530

    v-1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Todo from './to-do list'
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        
+      <Todo/>
     </div>
   )
 }
---
+++ src/components/to-do list.js
@@ -0,0 +1,58 @@
+
+import React,{useState} from "react";
+
+const Todo = ()=>{
+
+    const [todo,setTodo] = useState("");
+    const [TodoList,setTodoList] = useState([]);
+
+
+    const addTodo = ()=>{
+
+        if (todo.trim() !== "") {
+            setTodoList((prevTodos) => [...prevTodos, todo]);
+            setTodo("");
+        }
+    }
+
+
+    const deleteItem =(index) =>{
+        const updatedTodoList = [...TodoList];
+        updatedTodoList.splice(index, 1);
+        setTodoList(updatedTodoList);
+     
+    }
+
+    return(
+
+        <div className="main">
+
+           <center> <h2>To-Do List</h2></center>
+
+
+            <div>  
+            <input value={todo} onChange={(e)=>setTodo(e.target.value)} type="text" />
+            <button onClick={addTodo}>Add Todo</button>
+            </div> 
+
+
+            <ul>
+
+            {TodoList.map((todoItem, index) => (
+             <ul key={index}>{todoItem} <button onClick={()=>deleteItem(index)} className="delete">Delete</button></ul>
+            ))}
+                
+                
+            </ul>
+
+        </div>
+       
+
+        
+
+       
+    )
+
+}
+
+export default  Todo
